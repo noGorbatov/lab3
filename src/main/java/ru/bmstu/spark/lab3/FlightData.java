7ee969e30d2465be27f873f9fc636bd089ac217a
noGorbatov <gorbatov.nickita@yandex.ru>
2021-12-25 09:50:09 +0300

    Scripted auto-commit on change (2021-12-25 09:50:09) by gitwatch.sh

--- src/main/java/ru/bmstu/spark/lab3/FlightData.java
+++ src/main/java/ru/bmstu/spark/lab3/FlightData.java
@@ -3,10 +3,13 @@ package ru.bmstu.spark.lab3;
 import java.io.Serializable;
 
 public class FlightData implements Serializable {
-    private float delayTime;
-    private boolean delayed;
-    private boolean cancelled;
-    private float 
+    private float delayTime = 0;
+    private boolean delayed = false;
+    private boolean cancelled = false;
+    private float maxDelayTime = 0;
+    private int flightNumber = 0;
+    private int delayedNumber = 0;
+    private int cancelledNumber = 0;
     public FlightData(float delayTime, boolean delayed, boolean cancelled) {
         this.delayTime = delayTime;
         this.delayed = delayed;
@@ -20,5 +23,20 @@ public class FlightData implements Serializable {
                 ", delayed=" + delayed +
                 ", cancelled=" + cancelled +
                 '}';
+    }
+
+    public FlightData add(FlightData data) {
+        flightNumber++;
+        if (data.cancelled) {
+            cancelledNumber++;
+            return this;
+        }
+        if (data.delayed) {
+            delayedNumber++;
+            if (data.delayTime > maxDelayTime) {
+                maxDelayTime = data.delayTime;
+            }
+        }
+        return this;
     }
 }
