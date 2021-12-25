1bacb7f8c1fd5114771ad9105af72b43d7e9602b
noGorbatov <gorbatov.nickita@yandex.ru>
2021-12-25 09:27:41 +0300

    Scripted auto-commit on change (2021-12-25 09:27:41) by gitwatch.sh

--- src/main/java/ru/bmstu/spark/lab3/AirportApp.java
+++ src/main/java/ru/bmstu/spark/lab3/AirportApp.java
@@ -1,13 +1,14 @@
 package ru.bmstu.spark.lab3;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.rdd.RDD;
 
 public class AirportApp {
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName("lab3");
         JavaSparkContext sc = new JavaSparkContext(conf);
-        RDD<String> stats = sc.textFile("ids");
+        JavaRDD<String> stats = sc.textFile("ids");
+        
     }
 }
