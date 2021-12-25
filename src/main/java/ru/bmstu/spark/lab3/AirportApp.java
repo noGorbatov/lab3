9185d80d8f0016b8a2af96de824bda25fee49f62
noGorbatov <gorbatov.nickita@yandex.ru>
2021-12-25 09:44:28 +0300

    Scripted auto-commit on change (2021-12-25 09:44:28) by gitwatch.sh

--- src/main/java/ru/bmstu/spark/lab3/AirportApp.java
+++ src/main/java/ru/bmstu/spark/lab3/AirportApp.java
@@ -31,6 +31,6 @@ public class AirportApp {
                                         new FlightData(parsedData.getDelayTime(), parsedData.getDelayed(), parsedData.getCancelled()));
                 }
         );
-        System.out.println(statsRdd.take(5).forEach(System.out.println;));
+        System.out.println(statsRdd.take(5).forEach());
     }
 }
