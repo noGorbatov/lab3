df1575cde33d045a666c34b19502bde5685b4636
noGorbatov <gorbatov.nickita@yandex.ru>
2021-12-25 09:33:01 +0300

    Scripted auto-commit on change (2021-12-25 09:33:01) by gitwatch.sh

--- src/main/java/ru/bmstu/spark/lab3/AirportKey.java
+++ src/main/java/ru/bmstu/spark/lab3/AirportKey.java
@@ -1,4 +1,20 @@
 package ru.bmstu.spark.lab3;
 
-public class AirportKey {
+import java.io.Serializable;
+
+public class AirportKey implements Serializable {
+    private int srcAirport;
+    private int destAirport;
+    public AirportKey(int src, int dest) {
+        srcAirport = src;
+        destAirport = dest;
+    }
+
+    public int getDestAirport() {
+        return destAirport;
+    }
+
+    public int getSrcAirport() {
+        return srcAirport;
+    }
 }
