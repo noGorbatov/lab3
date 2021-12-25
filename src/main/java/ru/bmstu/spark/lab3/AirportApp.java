ed06970d9d8fd2152792336e62c43cbbde07bef0
noGorbatov <gorbatov.nickita@yandex.ru>
2021-12-25 09:53:40 +0300

    Scripted auto-commit on change (2021-12-25 09:53:40) by gitwatch.sh

--- src/main/java/ru/bmstu/spark/lab3/AirportApp.java
+++ src/main/java/ru/bmstu/spark/lab3/AirportApp.java
@@ -35,6 +35,6 @@ public class AirportApp {
 
         statsRdd.take(3).forEach(obj -> System.out.println(obj._1 + "\n" + obj._2 + "\n"));
 
-        JavaRDD<AirportKey, FlightData> 
+        JavaRDD<AirportKey, FlightData>
     }
 }
