be419e214021b19308a7ba56af699bcacf38d010
noGorbatov <gorbatov.nickita@yandex.ru>
2021-12-25 10:25:32 +0300

    Scripted auto-commit on change (2021-12-25 10:25:32) by gitwatch.sh

--- src/main/java/ru/bmstu/spark/lab3/AirportApp.java
+++ src/main/java/ru/bmstu/spark/lab3/AirportApp.java
@@ -55,7 +55,7 @@ public class AirportApp {
 //                }
 //        );
         JavaPairRDD<Tuple2<Integer, Integer>, FlightData> airportStats = statsRdd.combineByKey(
-                fData -> new FlightData(fData),
+                fData -> {return new FlightData(fData)},
                 (combiner, fData) -> combiner.add(fData.getCancelledPercent())
 
         )
