5d1febc74070ae724670d3d06d42b60a25fab867
noGorbatov <gorbatov.nickita@yandex.ru>
2021-12-25 09:38:21 +0300

    Scripted auto-commit on change (2021-12-25 09:38:21) by gitwatch.sh

--- src/main/java/ru/bmstu/spark/lab3/ParsedData.java
+++ src/main/java/ru/bmstu/spark/lab3/ParsedData.java
@@ -8,14 +8,14 @@ public class ParsedData implements Serializable {
     private int delayTime;
     private boolean delayed;
     private boolean cancelled;
-    private static String SPACE_SEP = " ";
+    private static String SPACE_SEP = ",";
     private static int SRC_AIRPORT_POS = 11;
     private static int DEST_AIRPORT_POS = 14;
 
     public static ParsedData parse(String line) {
         String[] data = line.split(SPACE_SEP);
         ParsedData res = new ParsedData();
-        res.srcAirport = data[SRC_AIRPORT_POS];
-        res
+        res.srcAirport = Integer.parseInt(data[SRC_AIRPORT_POS]);
+        res.destAirport = Integer.parseInt()
     }
 }
